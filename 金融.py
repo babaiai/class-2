21e6bcc165afeb375fc63bd981ff8105341d6cff
babaiai <168524852+babaiai@users.noreply.github.com>
2024-05-31 21:43:37 +0800

    Update 金融.py

--- 金融.py
+++ 金融.py
@@ -5,6 +5,15 @@ import streamlit as st
 # 从本地文件读取股票数据
 df = pd.read_csv('C:\\Users\\vicky\\Desktop\\學校\\333\\6560.csv')
 
+html_temp = """
+  <div style="background-color:#3872fb;padding:10px;border-radius:10px">
+  <h1 style="color:white;text-align:center;">雙邦金融資料視覺化呈現 (金融看板) </h1>
+  <h2 style="color:white;text-align:center;">Financial Dashboard </h2>
+  </div>
+  """
+stc.html(html_temp)
+
+
 # 计算移动平均线（MA）
 df['MA_20'] = df['close'].rolling(window=20).mean()
 
